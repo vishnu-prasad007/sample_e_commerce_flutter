021261eefa5baf4b67f42eef79f502569f80311c
Vishnu Prasad <vishnu007vprasad@gmail.com>
2021-04-05 19:21:48 +0530

    login screen

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class BrandColors {
+  static const Color tailColor = Color.fromRGBO(44, 191, 177, 1);
+  static const Color tailColorDark = Color.fromRGBO(3, 140, 115, 1);
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:sample_e_commerce_flutter/colors.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: BrandColors.tailColor,
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.only(left: 8.0, right: 8.0),
+            height: 315,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6.0)),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Logo Here',
+                      style: TextStyle(fontSize: 26.0),
+                    ),
+                    SizedBox(
+                      height: 26.0,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Your Username',
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: BrandColors.tailColorDark))),
+                    ),
+                    SizedBox(
+                      height: 18.0,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: 'Your Password',
+                          border: OutlineInputBorder(),
+                          suffixIcon: Icon(Icons.visibility_rounded,color: BrandColors.tailColorDark,),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: BrandColors.tailColorDark))),
+                    ),
+                    SizedBox(
+                      height: 18.0,
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.login),
+                      label: Text('Login'),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            BrandColors.tailColorDark),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
